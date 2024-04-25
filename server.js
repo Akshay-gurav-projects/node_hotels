bca6f23336b3eef0157633732b51d1970b9f786a
Akshay-gurav-projects <akshaygurav189@gmail.com>
2024-04-25 14:39:55 +0530

    comment add

--- server.js
+++ server.js
@@ -1,49 +1,3 @@
-// var fs = require('fs')
-// var os = require('os')
-// console.log(fs)
-// console.log(os)
-
-// const user = os.userInfo()
-// console.log(user)
-
-// fs.appendFile("greeting.txt","hi"+user.username,()=>{
-// 	console.log("flie is created")
-// })
-
-// ******************************************************************
-
-// const notes = require("./note.js")
-
-// ******************************************************************
-
-// const express = require('express')
-// const app = express()
-
-// app.get('/', function (req, res) {
-//   res.send('Hello welcome to my hotel')
-// })
-
-// app.get('/chicken',(req,res)=>{
-// 	res.send("Sure i will serve you butter chiken..")
-// })
-
-// app.get('/idli',(req,res)=>{
-// 	var custumized_idli = {
-// 		chutney : true,
-// 		sambar : false,
-// 		idli : 2,
-// 		vada : ["dal vada","medu vada"]
-// 	}
-// 	res.send(custumized_idli)
-// })
-
-// app.listen(3000,()=>{
-// 	console.log("server is running!!!!")
-// }) 
-
-
-// ******************************************************************
-
 const express = require('express')
 const app = express()
 const db = require('./db')
@@ -63,6 +17,7 @@ app.use('/person',personRouts);
 const menuItemRoutes = require('./routs/menuItemRoutes')
 app.use('/menuItems',menuItemRoutes);
 
+// this is server port Number
 app.listen(3000,()=>{
 	console.log("server is running!!!!")
 }) 
